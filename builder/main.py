33405239b1fef74703126db96aa7b09bddfd02d9
Greycode-za <skillsprogram@greycode.co.za>
2025-11-03 10:04:16 +0200

    Add custom build script for Greycode Platform

---
+++ builder/main.py
@@ -0,0 +1,7 @@
+# Custom build script for Greycode Platform
+# You can add pre/post build actions here later.
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+
+print("Building project using the Greycode IoT Platform...")
